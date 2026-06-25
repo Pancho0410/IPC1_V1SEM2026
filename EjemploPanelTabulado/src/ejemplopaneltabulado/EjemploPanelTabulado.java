365b7766b975910dd4aa763fb1da1213e8512a4b
Pancho0410 <tobiasf.zamora@gmail.com>
2026-06-24 19:30:46 -0600

    ejemplo hilo

---
+++ EjemploPanelTabulado/src/ejemplopaneltabulado/EjemploPanelTabulado.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplopaneltabulado;
+
+import vistas.PanelTabulado;
+import vistas.VistaAnimacion;
+public class EjemploPanelTabulado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PanelTabulado p = new PanelTabulado();
+        p.setVisible(true);
+        
+        /*int[][] resultaMuestra = {{1,2},{3,4}};
+        int[][] patron = {{1,2},{3,4}};
+        int dimsension = 2;
+        
+        VistaAnimacion animacion = new VistaAnimacion(dimsension,resultaMuestra,patron);
+        animacion.setVisible(true);*/
+    }
+    
+}
